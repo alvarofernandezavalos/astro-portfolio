16e533e5ac0e4560b7bc3f92642fabff7d18c864
Alvaro Fernandez <afernandez@s4gaming.com>
2023-12-30 21:30:25 +0100

    add SITE_DESCRIPTION

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Portfolio';
+export const SITE_TITLE = 'Alvaro Fernandez - Portfolio';
+export const SITE_DESCRIPTION = 'my personal portfolio';
